a67b3bec43223c4e72b4ecf867dba40acb70c2e2
icordero99 <ivan.cordero.uba@gmail.com>
2026-06-19 12:21:40 -0300

    Update SQL_Flex_Práctica1.sql

--- SQL_Flex_Práctica1.sql
+++ SQL_Flex_Práctica1.sql
@@ -1,16 +1,20 @@
--- EJERCICIO
+-- EJERCICIO CON INNER JOIN
 SELECT FirstName, DepartmentName FROM dbo.Employees AS e
 INNER JOIN dbo.Departments ON e.DepartmentID = dbo.Departments.DepartmentID
 
+-- EJERCICIO CON LEFT JOIN    
 SELECT FirstName, ProjectName FROM dbo.Employees AS e
 LEFT JOIN dbo.Projects ON e.EmployeeID = dbo.Projects.LeaderEmployeeID
 
+-- EJERCICIO CON RIGHT JOIN    
 SELECT ProjectName, FirstName FROM dbo.Employees AS e
 RIGHT JOIN dbo.Projects ON dbo.Projects.LeaderEmployeeID = e.EmployeeID
 
+-- EJERCICIO CON FULL JOIN    
 SELECT FirstName, EvalDate, Score FROM dbo.Employees AS e
 FULL JOIN dbo.Evaluations ON e.EmployeeID = dbo.Evaluations.EmployeeID
 
+-- EJERCICIO CON SUBCONSULTA
 SELECT EmployeeID, FirstName, LastName, DepartmentID FROM Employees
 WHERE DepartmentID IN (
     SELECT DepartmentID FROM Employees
