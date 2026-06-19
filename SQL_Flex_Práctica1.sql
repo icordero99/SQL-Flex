8bac021f15f2ecea703711058d38f1935f5e0fd7
icordero99 <ivan.cordero.uba@gmail.com>
2026-06-19 12:15:34 -0300

    Update SQL_Flex_Práctica1.sql

--- SQL_Flex_Práctica1.sql
+++ SQL_Flex_Práctica1.sql
@@ -1,3 +1,19 @@
--- EJERCICIO 1
+-- EJERCICIO
 SELECT FirstName, DepartmentName FROM dbo.Employees AS e
 INNER JOIN dbo.Departments ON e.DepartmentID = dbo.Departments.DepartmentID
+
+SELECT FirstName, ProjectName FROM dbo.Employees AS e
+LEFT JOIN dbo.Projects ON e.EmployeeID = dbo.Projects.LeaderEmployeeID
+
+SELECT ProjectName, FirstName FROM dbo.Employees AS e
+RIGHT JOIN dbo.Projects ON dbo.Projects.LeaderEmployeeID = e.EmployeeID
+
+SELECT FirstName, EvalDate, Score FROM dbo.Employees AS e
+FULL JOIN dbo.Evaluations ON e.EmployeeID = dbo.Evaluations.EmployeeID
+
+SELECT EmployeeID, FirstName, LastName, DepartmentID FROM Employees
+WHERE DepartmentID IN (
+    SELECT DepartmentID FROM Employees
+    GROUP BY DepartmentID
+    HAVING COUNT(*) > 10
+    );
